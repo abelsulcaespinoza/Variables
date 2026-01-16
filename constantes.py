e5e2fd7c629b0a5523314dc472690b62360b3396
Abel Sulca Espinoza <abelsulcaespinoza@gmail.com>
2026-01-16 16:49:55 -0500

    Update: constantes.py

---
+++ constantes.py
@@ -0,0 +1,15 @@
+import math
+
+print("**** Contantes en Python ****")
+
+PI = 3.1416
+GRAVEDAD = 9.81
+VELOCIDAD_LUZ = 299792458  # en metros por segundo
+AU = 149597870700  # Unidad Astronómica en metros
+
+print(f"Valor de PI: {PI:.2f}")
+print(f"Valor de la gravedad: {GRAVEDAD:.2f} m/s²")
+print(f"Velocidad de la luz: {VELOCIDAD_LUZ} m/s")
+print(f"Unidad Astronómica: {AU} metros")
+
+print(f"Valor de Math.pi:, {math.pi:.3f}")
