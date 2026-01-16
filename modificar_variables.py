7b7b7926ee7cb56f457e54a06694a5f192479033
Abel Sulca Espinoza <abelsulcaespinoza@gmail.com>
2026-01-16 16:03:46 -0500

    Update: Modificar Variables

---
+++ modificar_variables.py
@@ -0,0 +1,24 @@
+# Programar: Cabio de identidad del superhéroe
+
+# Asignar valores iniciales a las variables
+
+nombre = "El programador nocturno"
+nivel_energia = 85
+tiene_capa = False
+
+# Mostrar los valores iniciales
+print("Valores iniciales:")
+print("Nombre:", nombre)
+print("Nivel de energía:", nivel_energia)
+print("Tiene capa:", tiene_capa)
+
+# Modificar los valores de las variables
+nombre = "El programador diurno"
+nivel_energia = 105
+tiene_capa = True
+
+# Mostrar los valores modificados
+print("\nValores modificados:")
+print("Nombre:", nombre)
+print("Nivel de energía:", nivel_energia)
+print("Tiene capa:", tiene_capa)
