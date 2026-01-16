33bf5f29d25a525862454454d1c22ac3857c0a30
Abel Sulca Espinoza <abelsulcaespinoza@gmail.com>
2026-01-16 02:43:48 -0500

    Update: Tipo de Datos

---
+++ tipos_datos.py
@@ -0,0 +1,36 @@
+# Programa: Ficha secreta del superhéroe
+
+# Tipos de datos en Python
+
+# Str
+nombre = "Captain Python"
+alias = "El Guardián del Código"
+poder_principal = "Controlar los bugs con la mente"
+
+# Int
+edad = 28
+misiones_completadas = 42
+
+# Float
+nivel_energia = 99.8
+
+# Bool
+tiene_capa = True
+es_villano = False
+
+# NoneType
+compania_actual = None
+
+# Mostrar la ficha secreta del superhéroe
+print("Ficha Secreta del Superhéroe")
+print("----------------------------")
+print("Nombre:", nombre)
+print("Alias:", alias)
+print("Poder Principal:", poder_principal)
+print("Edad:", edad)
+print("Misiones Completadas:", misiones_completadas)
+print("Nivel de Energía:", nivel_energia)
+print("¿Tiene Capa?:", tiene_capa)
+print("¿Es Villano?:", es_villano)
+print("Compañía Actual:", compania_actual, type(compania_actual))
+# Fin del programa
